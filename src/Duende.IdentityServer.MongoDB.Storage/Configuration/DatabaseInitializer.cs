871adece9614e2c7bb7bcff9a94c046506335d9b
Brian Buvinghausen <buvinghausen@users.noreply.github.com>
2021-07-17 16:42:23 -0500

    Remove final vestiges of fixed collection names

--- src/Duende.IdentityServer.MongoDB.Storage/Configuration/DatabaseInitializer.cs
+++ src/Duende.IdentityServer.MongoDB.Storage/Configuration/DatabaseInitializer.cs
@@ -3,27 +3,34 @@ using System.Threading.Tasks;
 using Duende.IdentityServer.Models;
 using IdentityServer.MongoDB.Abstractions.Configuration;
 using IdentityServer.MongoDB.Abstractions.Options;
+using ConfigurationStoreOptions = Duende.IdentityServer.MongoDB.Storage.Options.ConfigurationStoreOptions;
 
 namespace Duende.IdentityServer.MongoDB.Storage.Configuration
 {
 	internal class DatabaseInitializer : DatabaseInitializerBase
 	{
+		private readonly string[] _additionalNames;
+
 		public DatabaseInitializer(ConfigurationStoreOptions configurationStoreOptions, OperationalStoreOptions operationalStoreOptions) : base(configurationStoreOptions, operationalStoreOptions)
 		{
+			_additionalNames = new[]
+			{
+				configurationStoreOptions.IdentityProviderCollectionName,
+				configurationStoreOptions.SigningKeyCollectionName
+			};
 		}
 
-		public DatabaseInitializer(ConfigurationStoreOptions configurationStoreOptions) : base(configurationStoreOptions)
+		public DatabaseInitializer(ConfigurationStoreOptions configurationStoreOptions) : this(configurationStoreOptions, default)
 		{
 		}
 
 		public DatabaseInitializer(OperationalStoreOptions operationalStoreOptions) : base(operationalStoreOptions: operationalStoreOptions)
 		{
+			_additionalNames = new string[0];
 		}
 
 		public override Task InitializeConfigurationStoreAsync(CancellationToken cancellationToken = default) =>
-			InitializeConfigurationStoreAsync<Client, Resource>(c => c.AllowedCorsOrigins, r => r.Name,
-				new[] { "IdentityProviders", "SigningKeys" },
-				cancellationToken);
+			InitializeConfigurationStoreAsync<Client, Resource>(c => c.AllowedCorsOrigins, r => r.Name, _additionalNames, cancellationToken);
 
 		public override Task InitializeOperationalStoreAsync(CancellationToken cancellationToken = default) =>
 			InitializeOperationalStoreAsync<DeviceFlowCode, PersistedGrant>(
