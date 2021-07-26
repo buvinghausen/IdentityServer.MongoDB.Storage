29da91c16744ead78baf0532ccb9973ba6064107
Brian Buvinghausen <buvinghausen@users.noreply.github.com>
2021-07-26 07:38:32 -0500

    Add initial IIdentityServerBuilder configuration

--- src/Duende.IdentityServer.MongoDB.Storage/Configuration/ServiceConfigurationExtensions.cs
+++ src/Duende.IdentityServer.MongoDB.Storage/Configuration/ServiceConfigurationExtensions.cs
@@ -3,8 +3,10 @@ using Duende.IdentityServer.Models;
 using Duende.IdentityServer.MongoDB.Storage.Admin;
 using Duende.IdentityServer.MongoDB.Storage.Configuration;
 using Duende.IdentityServer.MongoDB.Storage.Options;
+using Duende.IdentityServer.MongoDB.Storage.Stores;
 using IdentityServer.MongoDB.Abstractions.Admin;
 using IdentityServer.MongoDB.Abstractions.Configuration;
+using IdentityServer.MongoDB.Abstractions.Services;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
 // ReSharper disable once CheckNamespace
@@ -40,6 +42,60 @@ namespace Microsoft.Extensions.DependencyInjection
 				.AddSingleton(options)
 				.TryAddTransient<IDatabaseInitializer, DatabaseInitializer>();
 			return services;
+		}
+
+		public static IIdentityServerBuilder AddConfigurationStore(this IIdentityServerBuilder builder, Action<ConfigurationStoreOptions> config)
+		{
+			Initialize();
+			var options = new ConfigurationStoreOptions();
+			config(options);
+			builder.Services
+				.AddSingleton(options)
+				.TryAddTransient<IDatabaseInitializer, DatabaseInitializer>();
+
+			builder
+				.AddClientStore<MongoClientStore>()
+				.AddResourceStore<MongoResourceStore>()
+				.AddIdentityProviderStore<MongoIdentityProviderStore>()
+				.AddCorsPolicyService<MongoClientStore>();
+
+			if (!options.AddConfigurationStoreUpdaters)
+				return builder;
+
+			// Only add IConfigurationStoreUpdater to the service collection if explicitly requested
+			builder.Services
+				.AddTransient<IConfigurationStoreUpdater<Client>, MongoClientUpdater>()
+				.AddTransient<IConfigurationStoreUpdater<ApiResource>, MongoResourceUpdater<ApiResource>>()
+				.AddTransient<IConfigurationStoreUpdater<ApiScope>, MongoResourceUpdater<ApiScope>>()
+				.AddTransient<IConfigurationStoreUpdater<IdentityResource>, MongoResourceUpdater<IdentityResource>>();
+
+			return builder;
+		}
+
+		public static IIdentityServerBuilder AddOperationalStore(this IIdentityServerBuilder builder, Action<OperationalStoreOptions> config)
+		{
+			Initialize();
+			var options = new OperationalStoreOptions();
+			config(options);
+			builder.Services
+				.AddSingleton(options)
+				.TryAddTransient<IDatabaseInitializer, DatabaseInitializer>();
+
+			builder
+				.AddPersistedGrantStore<MongoPersistedGrantStore>()
+				.AddDeviceFlowStore<MongoDeviceFlowStore>()
+				.AddSigningKeyStore<MongoSigningKeyStore>();
+
+			if (!options.EnableTokenCleanup) return builder;
+
+			// Only add TokenCleanupService if explicitly requested
+			builder.Services
+				.AddHostedService(_ => new TokenCleanupService(options, new IOperationalStore[]
+				{
+					new MongoDeviceFlowStore(options),
+					new MongoPersistedGrantStore(options)
+				}));
+			return builder;
 		}
 
 		private static void Initialize()
--- src/IdentityServer4.MongoDB.Storage/Configuration/ServiceConfigurationExtensions.cs
+++ src/IdentityServer4.MongoDB.Storage/Configuration/ServiceConfigurationExtensions.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using IdentityServer.MongoDB.Abstractions.Admin;
 using IdentityServer.MongoDB.Abstractions.Configuration;
+using IdentityServer.MongoDB.Abstractions.Services;
 using IdentityServer4.Models;
 using IdentityServer4.MongoDB.Storage.Admin;
 using IdentityServer4.MongoDB.Storage.Configuration;
 using IdentityServer4.MongoDB.Storage.Options;
+using IdentityServer4.MongoDB.Storage.Stores;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
 // ReSharper disable once CheckNamespace
@@ -41,6 +43,59 @@ namespace Microsoft.Extensions.DependencyInjection
 				.TryAddTransient<IDatabaseInitializer, DatabaseInitializer>();
 			return services;
 		}
+
+		public static IIdentityServerBuilder AddConfigurationStore(this IIdentityServerBuilder builder, Action<ConfigurationStoreOptions> config)
+		{
+			Initialize();
+			var options = new ConfigurationStoreOptions();
+			config(options);
+			builder.Services
+				.AddSingleton(options)
+				.TryAddTransient<IDatabaseInitializer, DatabaseInitializer>();
+
+			builder
+				.AddClientStore<MongoClientStore>()
+				.AddResourceStore<MongoResourceStore>()
+				.AddCorsPolicyService<MongoClientStore>();
+
+			if (!options.AddConfigurationStoreUpdaters)
+				return builder;
+
+			// Only add IConfigurationStoreUpdater to the service collection if explicitly requested
+			builder.Services
+				.AddTransient<IConfigurationStoreUpdater<Client>, MongoClientUpdater>()
+				.AddTransient<IConfigurationStoreUpdater<ApiResource>, MongoResourceUpdater<ApiResource>>()
+				.AddTransient<IConfigurationStoreUpdater<ApiScope>, MongoResourceUpdater<ApiScope>>()
+				.AddTransient<IConfigurationStoreUpdater<IdentityResource>, MongoResourceUpdater<IdentityResource>>();
+
+			return builder;
+		}
+
+		public static IIdentityServerBuilder AddOperationalStore(this IIdentityServerBuilder builder, Action<OperationalStoreOptions> config)
+		{
+			Initialize();
+			var options = new OperationalStoreOptions();
+			config(options);
+			builder.Services
+				.AddSingleton(options)
+				.TryAddTransient<IDatabaseInitializer, DatabaseInitializer>();
+
+			builder
+				.AddPersistedGrantStore<MongoPersistedGrantStore>()
+				.AddDeviceFlowStore<MongoDeviceFlowStore>();
+
+			if (!options.EnableTokenCleanup) return builder;
+
+			// Only add TokenCleanupService if explicitly requested
+			builder.Services
+				.AddHostedService(_ => new TokenCleanupService(options, new IOperationalStore[]
+				{
+					new MongoDeviceFlowStore(options),
+					new MongoPersistedGrantStore(options)
+				}));
+			return builder;
+		}
+
 
 		private static void Initialize()
 		{
