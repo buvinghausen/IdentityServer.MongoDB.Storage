f4adf9a2930832ce5aa48bdf9453b2e3094cfaf7
Brian Buvinghausen <buvinghausen@users.noreply.github.com>
2021-09-15 21:05:31 -0500

    Use top level statements for workers too

--- samples/IdentityServer4.AdminConsole/Program.cs
+++ samples/IdentityServer4.AdminConsole/Program.cs
@@ -1,20 +1,18 @@
-﻿namespace IdentityServer4.AdminConsole;
+﻿using IdentityServer4.AdminConsole;
 
-internal class Program
-{
-	private static Task Main(string[] args) =>
-		Host.CreateDefaultBuilder(args)
-			.ConfigureServices((context, services) =>
-			{
-				// Wire up otel (not required) and point identityserver admin to the desired database
-				// Note: operational store and configuration stores can be in separate databases
-				// You will need write access to both stores from the admin console
-				var database = context.Configuration.GetMongoDatabase("Identity");
-				services
-					.AddOpenTelemetryTracing(context.HostingEnvironment.ApplicationName)
-					.AddIdentityServerConfigurationStoreAdmin(options => options.Database = database)
-					.AddIdentityServerOperationalStoreAdmin(options => options.Database = database)
-					.AddHostedService<DatabaseInitializer>();
-			})
-			.RunConsoleAsync();
-}
+await Host
+	.CreateDefaultBuilder(args)
+	.ConfigureServices((context, services) =>
+	{
+		// Wire up otel (not required) and point identityserver admin to the desired database
+		// Note: operational store and configuration stores can be in separate databases
+		// You will need write access to both stores from the admin console
+		var database = context.Configuration.GetMongoDatabase("Identity");
+		services
+			.AddOpenTelemetryTracing(context.HostingEnvironment.ApplicationName)
+			.AddIdentityServerConfigurationStoreAdmin(options => options.Database = database)
+			.AddIdentityServerOperationalStoreAdmin(options => options.Database = database)
+			.AddHostedService<DatabaseInitializer>();
+	})
+	.RunConsoleAsync()
+	.ConfigureAwait(false);
--- samples/IdentityServer4.Web/Program.cs
+++ samples/IdentityServer4.Web/Program.cs
@@ -14,7 +14,8 @@ builder.Services
 		options.EnableTokenCleanup = true; // <-- turn on IHostedService which will periodically wipe PersistedGrants & DeviceCodes (defaults to false)
 		options.RemoveConsumedTokens = true; // <-- cleanup consumed tokens in addition to expired tokens (defaults to false)
 	});
-builder.Services.AddControllers();
+builder.Services
+	.AddControllers();
 var app = builder.Build();
 if (builder.Environment.IsDevelopment()) app.UseDeveloperExceptionPage();
 app
